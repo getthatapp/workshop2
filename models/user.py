c30a06a7c1cb7b5e65f69ab0ca342502c26466de
Grzegorz Turek <g.turek@yahoo.com>
2024-08-17 21:45:52 +0200

    queries

---
+++ models/user.py
@@ -0,0 +1,33 @@
+from common.crypto import hashPassword
+from queries.sqlQueries import Query
+
+
+class User:
+
+    def __init__(self, userId=None, username=None, password=None, salt=None):
+        self.userId = userId
+        self.username = username
+        self.hashedPassword = hashPassword(password, salt)
+
+    @property
+    def id(self):
+        return self.userId
+
+    @property
+    def hashedPassword(self):
+        return self.hashedPassword
+
+    def setPassword(self, password, salt=None):
+        self.hashedPassword = hashPassword(password, salt)
+
+    @hashedPassword.setter
+    def hashedPassword(self, password):
+        self.setPassword(password)
+
+    def saveToDb(self, username, hashedPassword):
+        if self.id is None:
+            Query.saveUserToDatabase(username, hashedPassword)
+        else:
+            Query.updateUser(id, username, hashedPassword)
+
+
--- queries/sqlQueries.py
+++ queries/sqlQueries.py
@@ -1,4 +1,35 @@
+from common.dbSettings import dbSettings
+from psycopg2 import connect
+
+
 class Query:
 
+    def executeQuery(query):
+        conn = connect(**dbSettings)
+        conn.autocommit = True
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            try:
+                result = cursor.fetchall()
+            except:
+                result = None
+            conn.close()
+            return result
+
+    def updateSQL(query):
+        conn = connect(**dbSettings)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+
     def saveUserToDatabase(username, hashedPassword):
-        userQuery = 
+        saveUser = "INSERT INTO users(username, hashedPassword) VALUES(%s, %s) RETURNING id" % (username, hashedPassword)
+        result = Query.executeQuery(saveUser)
+        return result
+
+    def updateUser(username, hashedPassword, id):
+        updateUser = "UPDATE users SET username=%s, hashedPassword=%s WHERE id=%s" % (username, hashedPassword, id)
+        result = Query.executeUpdate(updateUser)
+        return result
+
